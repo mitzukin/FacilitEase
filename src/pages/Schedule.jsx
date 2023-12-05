632a4f631296e13b6db72cfba59185b3d02cfb9e
Jason Tajor <jasontajor0@gmail.com>
2023-12-05 23:13:50 +0800

    Initial commit

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -5,7 +5,7 @@ import Calendar from "../components/calendar";
 import Time from "../components/time";
 import { db } from "../firebase";
 import { ref, push, get } from "firebase/database";
-
+import { useMemo } from "react";
 const Schedule = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedTime, setSelectedTime] = useState(null);
@@ -14,18 +14,11 @@ const Schedule = () => {
   const [selectedDay, setSelectedDay] = useState(null);
   const [selectedMonth, setSelectedMonth] = useState(null);
 
-  const itemsPerPage = 20; // Adjust the number of items per page as needed
+  const itemsPerPage = 20;
   const [currentPage, setCurrentPage] = useState(1);
 
-  // Calculate the total number of pages
-  const totalPages = Math.ceil(appointments.length / itemsPerPage);
+ 
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentAppointments = appointments.slice(
-    indexOfFirstItem,
-    indexOfLastItem
-  );
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
@@ -43,10 +36,8 @@ const Schedule = () => {
   };
 
   const handleSubmit = async () => {
-    // Check if already submitting
     if (loading) return;
 
-    // Set loading state to true
     setLoading(true);
 
     try {
@@ -54,7 +45,6 @@ const Schedule = () => {
         const formattedDate = selectedDate.toISOString();
         const formattedTime = selectedTime.format("HH:mm");
 
-        // Push the appointment data to the "appointments" node in Firebase
         const appointmentsRef = ref(db, "appointments");
         await push(appointmentsRef, {
           date: formattedDate,
@@ -67,7 +57,6 @@ const Schedule = () => {
           formattedTime
         );
 
-        // Fetch the updated list of appointments after submission
         const updatedAppointmentsRef = ref(db, "appointments");
         const snapshot = await get(updatedAppointmentsRef);
 
@@ -75,6 +64,7 @@ const Schedule = () => {
           const appointmentsData = [];
           snapshot.forEach((childSnapshot) => {
             const appointment = childSnapshot.val();
+            appointment.date = new Date(appointment.date); // Parse the date string to a Date object
             appointmentsData.push(appointment);
           });
 
@@ -88,7 +78,6 @@ const Schedule = () => {
     } catch (error) {
       console.error("Error submitting appointment:", error.message);
     } finally {
-      // Set loading state back to false regardless of success or failure
       setLoading(false);
     }
   };
@@ -105,6 +94,7 @@ const Schedule = () => {
           const appointmentsData = [];
           snapshot.forEach((childSnapshot) => {
             const appointment = childSnapshot.val();
+            appointment.date = new Date(appointment.date); // Parse the date string to a Date object
             appointmentsData.push(appointment);
           });
 
@@ -122,13 +112,36 @@ const Schedule = () => {
     fetchAppointments();
   }, []); // Run once on component mount
 
+  // Filter appointments based on selected day and month
+  const filteredAppointments = useMemo(() => {
+    return appointments.filter((appointment) => {
+      if (selectedDay !== null && selectedDay !== "" && appointment.date.getDay() !== parseInt(selectedDay)) {
+        return false;
+      }
+
+      if (selectedMonth !== null && selectedMonth !== "" && (appointment.date.getMonth() + 1) !== parseInt(selectedMonth)) {
+        return false;
+      }
+
+      return true;
+    });
+  }, [appointments, selectedDay, selectedMonth]);
+
+  const totalPages = Math.ceil(filteredAppointments.length / itemsPerPage);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentAppointments = filteredAppointments.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
-    <div className="flex ">
+    <div className="flex">
+      {/* Sidebar component */}
       <div className="absolute z-10 bg-secondary">
         <Sidebar />
       </div>
-      <div className="relative flex-1 p-2 ml-12 md:p-8 ">
-        <div className="p-5 md:mx-10 lg:p-16 ">
+
+      <div className="relative flex-1 p-2 ml-12 md:p-8">
+        <div className="p-5 md:mx-10 lg:p-16">
           <h1 className="mb-4 text-2xl font-semibold md:text-4xl font-roboto">
             Make an Appointment
           </h1>
@@ -139,8 +152,10 @@ const Schedule = () => {
 
           <div className="flex flex-col justify-center gap-2 mt-10 xl:flex-row">
             <div className="flex flex-col-reverse xl:flex-col lg:w-full xl:w-1/4">
+              {/* Calendar component */}
               <Calendar onDateChange={handleDateChange} className="shadow-lg" />
 
+              {/* Appointment guide */}
               <div className="p-5 text-sm border rounded">
                 <h1 className="mb-5 text-xl font-semibold font-roboto">
                   Setting an Appointment Guide
@@ -153,9 +168,12 @@ const Schedule = () => {
                 ))}
               </div>
             </div>
-            <div className="ml-10 xl:w-3/4 ">
+
+            <div className="ml-10 xl:w-3/4">
+              {/* Time component */}
               <Time onTimeChange={handleTimeChange} />
 
+              {/* Submit button */}
               <button
                 onClick={handleSubmit}
                 className={`px-4 py-3 mt-4  rounded-md text-secondary text-sm font-roboto bg-primary ${
@@ -165,62 +183,62 @@ const Schedule = () => {
               >
                 {loading ? "Submitting..." : "Submit Appointment"}
               </button>
-              <div>
-                <div className="flex flex-col gap-2 lg:flex-row">
-                  <h2 className="mt-10 mb-5 text-2xl font-semibold font-roboto">
-                    Appointments Schedule:
-                  </h2>
-                  {/*For Day*/}
-                  <div className="mt-4">
-                    <label className="block text-sm font-roboto text-shade">
-                      Filter by Day:
-                    </label>
-                    <select
-                      onChange={(e) => handleDayChange(e.target.value)}
-                      value={selectedDay || ""}
-                      className="block w-full px-3 py-2 mt-1 bg-white border rounded-md shadow-sm border-shade focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
-                    >
-                      <option value="">All Days</option>
-                      <option value="Sun">Sun</option>
-                      <option value="Mon">Mon</option>
-                      <option value="Tue">Tue</option>
-                      <option value="Wed">Wed</option>
-                      <option value="Thu">Thu</option>
-                      <option value="Fri">Fri</option>
-                      <option value="Sat">Sat</option>
-                    </select>
-                  </div>
 
-                  {/* Filter for Month */}
-                  <div className="mt-4">
-                    <label className="block text-sm font-roboto text-shade">
-                      Filter by Month:
-                    </label>
-                    <select
-                      onChange={(e) => handleMonthChange(e.target.value)}
-                      value={selectedMonth || ""}
-                      className="block w-full px-3 py-2 mt-1 bg-white border rounded-md shadow-sm border-shade focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
-                    >
-                      <option value="">All Months</option>
-                      <option value="01">Jan</option>
-                      <option value="02">Feb</option>
-                      <option value="03">Mar</option>
-                      <option value="04">Apr</option>
-                      <option value="05">May</option>
-                      <option value="06">Jun</option>
-                      <option value="07">Jul</option>
-                      <option value="08">Aug</option>
-                      <option value="09">Sep</option>
-                      <option value="10">Oct</option>
-                      <option value="11">Nov</option>
-                      <option value="12">Dec</option>
-                    </select>
-                  </div>
+              {/* Filters for day and month */}
+              <div className="flex flex-col gap-2 lg:flex-row">
+                {/* Filter by Day */}
+                <div className="mt-4">
+                  <label className="block text-sm font-roboto text-shade">
+                    Filter by Day:
+                  </label>
+                  <select
+                    onChange={(e) => handleDayChange(e.target.value)}
+                    value={selectedDay || ""}
+                    className="block w-full px-3 py-2 mt-1 bg-white border rounded-md shadow-sm border-shade focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+                  >
+                    <option value="">All Days</option>
+                    <option value="0">Sun</option>
+                    <option value="1">Mon</option>
+                    <option value="2">Tue</option>
+                    <option value="3">Wed</option>
+                    <option value="4">Thu</option>
+                    <option value="5">Fri</option>
+                    <option value="6">Sat</option>
+                  </select>
                 </div>
-                {loading ? (
-                  <p>Retrieving Appointments...</p>
-                ) : (
-                  <div className="mt-4">
+
+                {/* Filter by Month */}
+                <div className="mt-4">
+                  <label className="block text-sm font-roboto text-shade">
+                    Filter by Month:
+                  </label>
+                  <select
+                    onChange={(e) => handleMonthChange(e.target.value)}
+                    value={selectedMonth || ""}
+                    className="block w-full px-3 py-2 mt-1 bg-white border rounded-md shadow-sm border-shade focus:outline-none focus:ring-primary focus:border-primary sm:text-sm"
+                  >
+                    <option value="">All Months</option>
+                    <option value="1">Jan</option>
+                    <option value="2">Feb</option>
+                    <option value="3">Mar</option>
+                    <option value="4">Apr</option>
+                    <option value="5">May</option>
+                    <option value="6">Jun</option>
+                    <option value="7">Jul</option>
+                    <option value="8">Aug</option>
+                    <option value="9">Sep</option>
+                    <option value="10">Oct</option>
+                    <option value="11">Nov</option>
+                    <option value="12">Dec</option>
+                  </select>
+                </div>
+              </div>
+
+              {/* Display filtered appointments */}
+              {loading ? (
+                <p>Retrieving Appointments...</p>
+              ) : (
+                <div className="mt-4">
                   <div className="grid grid-cols-1 gap-2 p-2 overflow-y-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 font-roboto">
                     {currentAppointments.map((appointment, index) => {
                       const formattedDate = new Intl.DateTimeFormat("en-US", {
@@ -228,14 +246,12 @@ const Schedule = () => {
                         month: "2-digit",
                         day: "2-digit",
                       }).format(new Date(appointment.date));
-            
+
                       const time = appointment.time.split(":");
                       const formattedTime = `${
                         parseInt(time[0], 10) % 12 || 12
-                      }:${time[1]} ${
-                        parseInt(time[0], 10) < 12 ? "AM" : "PM"
-                      }`;
-            
+                      }:${time[1]} ${parseInt(time[0], 10) < 12 ? "AM" : "PM"}`;
+
                       return (
                         <div
                           key={index}
@@ -246,24 +262,27 @@ const Schedule = () => {
                       );
                     })}
                   </div>
-            
+
                   {/* Pagination Controls */}
                   <div className="flex justify-center mt-4">
-                    {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-                      <button
-                        key={page}
-                        onClick={() => setCurrentPage(page)}
-                        className={`mx-1 px-3 py-2 rounded ${
-                          page === currentPage ? "bg-primary text-secondary" : "bg-secondary text-primary hover:bg-primary hover:text-secondary"
-                        }`}
-                      >
-                        {page}
-                      </button>
-                    ))}
+                    {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+                      (page) => (
+                        <button
+                          key={page}
+                          onClick={() => setCurrentPage(page)}
+                          className={`mx-1 px-3 py-2 rounded ${
+                            page === currentPage
+                              ? "bg-primary text-secondary"
+                              : "bg-secondary text-primary hover:bg-primary hover:text-secondary"
+                          }`}
+                        >
+                          {page}
+                        </button>
+                      )
+                    )}
                   </div>
                 </div>
-                )}
-              </div>
+              )}
             </div>
           </div>
         </div>
